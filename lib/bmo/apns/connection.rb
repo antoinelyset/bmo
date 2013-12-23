0eb67c7ddf9e5a7ca2362fc5967bcec241bcb8a2
antoinelyset <antoinelyset+github@gmail.com>
2013-12-23 11:47:31 +0100

    Simplify TCPSocket and SSLSocket Management, thanks sync_close (undocumented feature)

--- lib/bmo/apns/connection.rb
+++ lib/bmo/apns/connection.rb
@@ -16,55 +16,27 @@ module BMO
       #   a pure TCPSocket. It then yields the socket and handles the closing
       #
       def connect(&block)
-        if cert_path
-          connect_with_ssl do |socket|
-            yield socket
-          end
-        else
-          connect_without_ssl do |socket|
-            yield socket
-          end
-        end
+        socket = cert_path ? ssl_socket : tcp_socket
+        socket.connect
+
+        yield socket
+
+        socket.close
       end
 
       private
 
-      # Create a tcp and a ssl socket to the Apple Gateway,
-      #   connect to the ssl and yield the SSL socket,
-      #   it handles socket closing after yield
-      #
-      def connect_with_ssl(&block)
-        ssl_socket, tcp_socket = ssl_connection
-        ssl_socket.connect
-
-        yield ssl_socket
-
-        ssl_socket.close
-        tcp_socket.close
-      end
-
-      # Create a pure TCPSocket and yield it,
-      #   it handles socket closing after yield
-      #
-      def connect_without_ssl(&block)
-        tcp_socket = tcp_connection
-
-        yield tcp_socket
-
-        tcp_socket.close
-      end
-
       # @return [TCPSocket]
-      def tcp_connection
+      def tcp_socket
         TCPSocket.new(host, port)
       end
 
-      # @return [Array<SSLSocket, TCPSocket>] An array with first
-      #   the SSLSocket you should write to then the TCPSocket.
-      def ssl_connection
-        tcp_socket = tcp_connection
+      # @return [SSLSocket] the SSLSocket setted to sync_close
+      #   to sync tcp and ssl closing
+      def ssl_socket
         ssl_socket = OpenSSL::SSL::SSLSocket.new(tcp_socket, ssl_context)
-        [ssl_socket, tcp_socket]
+        ssl_socket.sync_close = true
+        ssl_socket
       end
 
       # @return [SSLContext] SSLContext setted with the certificate
